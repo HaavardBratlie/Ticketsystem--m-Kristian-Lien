b3e41f6c05b242e49c435f03ab29fcf4c0c4cf05
Aayan Aqdas <80333428+aayanaqdas@users.noreply.github.com>
2025-05-06 14:05:16 +0200

    startet med status tabellen på status siden

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import mysql.connector
 
 app = Flask(__name__)
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="10.2.3.108",
-        user="haavard",
-        password="1234567",
-        database="ticket_system"
+        # host="10.2.3.108",
+        # user="haavard",
+        # password="1234567",
+        # database="ticket_system"
+        host="127.0.0.1",
+        user = "flaskuser",
+        password = "password",
+        database = "ticketsystem"
     )
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/tickets")
-def tickets():
+@app.route("/henvendelser")
+def hendbendelser():
     return render_template("henvendelser.html")
 
 @app.route("/send")
@@ -27,9 +31,28 @@ def send():
 def status():
     return render_template("status.html")
 
-@app.route("/confirm")
-def confirm():
+@app.route("/bekreft")
+def bekreft():
     return render_template("bekreft.html")
+
+@app.route('/api/tickets', methods=['GET'])
+def get_tickets():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM tickets")
+        rows = cursor.fetchall()
+        cursor.close()
+
+        tickets = []
+
+        for row in rows:
+            tickets.append(row)
+
+        return jsonify({'status': 'success', 'message': 'Retrieved tickets', 'tickets': tickets}), 200
+    
+    except Exception as e:
+         return jsonify({'status': 'error', 'message': 'Failed to retrieve tickets', 'error': str(e)}), 500
 
 
 if __name__ == '__main__':
--- db_skjema.sql
+++ db_skjema.sql
@@ -4,6 +4,12 @@ CREATE TABLE tickets (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     message TEXT NOT NULL,
+    
     status ENUM('ikke påbegynt', 'under behandling', 'oppklart') DEFAULT 'ikke påbegynt',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Legger til en test rad i tickets-tabellen
+INSERT INTO tickets (ticket_id, name, email, message, status) 
+VALUES (1, 'Test Bruker', 'test@example.com', 'Dette er en testmelding.', 'ikke påbegynt');
+
